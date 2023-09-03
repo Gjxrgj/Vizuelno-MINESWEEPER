7a0343c1da03a0c8cd46ee5494fc651faa68d0f2
niknacevski <73705046+niknacevski@users.noreply.github.com>
2023-09-03 00:25:39 -0400

    Update Scene.cs
    Updated Scene class

--- WindowsFormsApp1/Scene.cs
+++ WindowsFormsApp1/Scene.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -24,6 +24,11 @@ namespace WindowsFormsApp1
         }
         public void DrawAll(Graphics graphics)
         {
+            DrawBackground(graphics);
+            foreach (Number number in Numbers)
+            {
+                number.Draw(graphics);
+            }
             foreach (Box box in Boxes)
             {
                 box.Draw(graphics);
@@ -32,20 +37,78 @@ namespace WindowsFormsApp1
             {
                 bomb.Draw(graphics);
             }
-            foreach (Number number in Numbers)
-            {
-                number.Draw(graphics);
-            }
             foreach (Flag flag in Flags)
             {
                 flag.Draw(graphics);
             }
+            
+        }
+        public void DrawBackground(Graphics g)
+        { 
+            // Define the starting coordinates and box dimensions
+            int startX = 60;
+            int startY = 30;
+            int boxSize = 30;
+            int rowCount = 9;
+            int colCount = 9;
+
+            // Define the outline color
+            Pen outlinePen = Pens.Black;
+
+            for (int row = 0; row < rowCount; row++)
+            {
+                for (int col = 0; col < colCount; col++)
+                {
+                    // Calculate the coordinates of the current box
+                    int x = startX + col * boxSize;
+                    int y = startY + row * boxSize;
+
+                    // Create a rectangle for the current box
+                    Rectangle boxRect = new Rectangle(x, y, boxSize, boxSize);
+
+                    // Fill the box with light gray color
+                    g.FillRectangle(Brushes.LightGray, boxRect);
+
+                    // Draw the outline for the box
+                    g.DrawRectangle(outlinePen, boxRect);
+                }
+            }
         }
         public bool Gameover()
         {
-            if (Boxes.Count == 10)
+            int UnclickedBoxes = 0;
+            foreach(Box box in Boxes)
+            {
+                if (!box.clicked)
+                {
+                    UnclickedBoxes++;
+                }
+            }
+            if (UnclickedBoxes == 10)
                 return true;
             return false;
+        }
+        public void RemoveFlagsOnClickedSpaces()
+        {
+            List<Flag> flagsToRemove = new List<Flag>();
+
+            foreach (Flag flag in Flags)
+            {
+                foreach (Box box in Boxes)
+                {
+                    if (box.clicked)
+                    {
+                        flagsToRemove.Add(flag);
+                        break; // No need to check this flag against other boxes
+                    }
+                }
+            }
+
+            // Remove the flags
+            /*foreach (Flag flagToRemove in flagsToRemove)
+            {
+                Flags.Remove(flagToRemove);
+            }*/
         }
         public void AddOrRemoveFlag(int closestX, int closestY, int actualX, int actualY)
         {
@@ -63,7 +126,7 @@ namespace WindowsFormsApp1
             {
                 foreach(Box box in Boxes)
                 {
-                    if (box.IsClicked(actualX, actualY)) {
+                    if (box.IsClicked(actualX, actualY) && box.clicked == false) {
                         Flags.Add(new Flag(closestX, closestY));
                     }
                 }
@@ -92,14 +155,98 @@ namespace WindowsFormsApp1
         }
         public void ClickBox(int x, int y)
         {
-            for(int i = 0; i < Boxes.Count; i++)
+            Box clickedBox = Boxes.FirstOrDefault(box => box.IsClicked(x, y));
+
+            if (clickedBox != null && !clickedBox.clicked)
             {
-                if(Boxes[i].IsClicked(x, y))
+                if (ClickBomb(x, y))
                 {
-                    Boxes.RemoveAt(i);
-                    i--;
+
+                    foreach (Bomb bomb in Bombs)
+                    {
+                        bomb.Covered = false;
+                    }
+                }
+                else if (clickedBox.Number == 0)
+                {
+                    HashSet<Box> boxesToClick = new HashSet<Box>();
+
+                    RecursiveClickZeros(clickedBox, boxesToClick);
+
+                    foreach (Box boxToClick in boxesToClick)
+                    {
+                        boxToClick.clicked = true;
+                        boxToClick.Color = Color.White;
+                    }
+                }
+                else
+                {
+                    clickedBox.clicked = true;
+                    clickedBox.Color = Color.White;
                 }
             }
+        }
+        private void RecursiveClickZeros(Box box, HashSet<Box> clickedBoxes)
+        {
+            clickedBoxes.Add(box);
+
+            foreach (Box adjacentBox in GetAdjacentBoxes(box.X, box.Y))
+            {
+                if (!clickedBoxes.Contains(adjacentBox))
+                {
+                    if (adjacentBox.Number == 0)
+                    {
+                        // If adjacent box is empty, recursively click it
+                        RecursiveClickZeros(adjacentBox, clickedBoxes);
+                    }
+                    else if (adjacentBox.Number > 0)
+                    {
+                        // If adjacent box has a number, click it and stop recursion
+                        adjacentBox.clicked = true;
+                    }
+                }
+            }
+        }
+
+
+
+        private List<Box> GetAdjacentBoxes(int x, int y)
+        {
+            List<Box> adjacentBoxes = new List<Box>();
+
+            if (IsWithinGridBounds(x, y - 30))
+            {
+                Box aboveBox = GetBoxAtCoordinates(x, y - 30);
+                adjacentBoxes.Add(aboveBox);
+            }
+
+            if (IsWithinGridBounds(x, y + 30))
+            {
+                Box belowBox = GetBoxAtCoordinates(x, y + 30);
+                adjacentBoxes.Add(belowBox);
+            }
+
+            if (IsWithinGridBounds(x - 30, y))
+            {
+                Box leftBox = GetBoxAtCoordinates(x - 30, y);
+                adjacentBoxes.Add(leftBox);
+            }
+
+            if (IsWithinGridBounds(x + 30, y))
+            {
+                Box rightBox = GetBoxAtCoordinates(x + 30, y);
+                adjacentBoxes.Add(rightBox);
+            }
+
+            return adjacentBoxes;
+        }
+        private bool IsWithinGridBounds(int x, int y)
+        {
+            return x >= 60 && x <= 300 && y >= 30 && y <= 270;
+        }
+        private Box GetBoxAtCoordinates(int boxX, int boxY)
+        {
+            return Boxes.FirstOrDefault(box => box.X == boxX && box.Y == boxY);
         }
         public bool ClickBomb(int x, int y)
         {
@@ -117,41 +264,41 @@ namespace WindowsFormsApp1
             int xMargin = 70;
             int x = xMargin;
             int y = 15;
-            int j = 0;
+            int xBomb = 30;
+            int yBomb = 0;
             for (int i = 0; i < 81; i++)
             {
-                j++;
-                if (j > 9)
-                {
-                    j = 0;
-                }
-                x += j * 30;
-                //Reset for X coordinate
-                if (x > 330)
+                x += 30;
+                xBomb += 30;
+
+                if ((i + 1) % 9 == 0)
                 {
                     x = xMargin;
+                    xBomb = 60;
                 }
                 int num = 0;
-                if(i % 9 == 0)
+                if (i % 9 == 0)
                 {
                     y += 30;
+                    yBomb += 30;
                 }
-                // Find if the number should be increased
-                foreach(Bomb bomb in Bombs)
+
+                foreach (Bomb bomb in Bombs)
                 {
                     if (bomb.IsAdjacent(x, y))
                     {
                         num++;
                     }
                 }
+
                 bool AddNumber = true;
                 Console.WriteLine("X coordinate::" + x);
-                //Find if the number being added is on top of a bomb
+
                 foreach (Bomb bomb in Bombs)
                 {
-                    if(bomb.IsColliding(x, y))
+                    if (bomb.IsColliding(x, y))
                     {
-                       
+
                         AddNumber = false;
                         break;
                     }
@@ -159,6 +306,13 @@ namespace WindowsFormsApp1
                 if (AddNumber)
                 {
                     Numbers.Add(new Number(x, y, num));
+                    foreach (Box box in Boxes)
+                    {
+                        if (box.X == xBomb && box.Y == yBomb)
+                        {
+                            box.Number = num;
+                        }
+                    }
                 }
             }
         }
