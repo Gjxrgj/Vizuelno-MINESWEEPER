1042d4b63b83c7e913335cc03608c40518c49d62
Marko <97228383+Gjxrgj@users.noreply.github.com>
2023-09-03 00:23:23 -0400

    Update Box.cs
    Added functions

--- WindowsFormsApp1/Box.cs
+++ WindowsFormsApp1/Box.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -13,21 +13,38 @@ namespace WindowsFormsApp1
         public int X { get; set; }
         public int Y { get; set; }
         public Color Color { get; set; }
+        public int Number { get; set; }
+        public bool clicked { get; set; }
 
         public Box(int X, int Y)
         {
             this.X = X;
             this.Y = Y;
             this.Color = Color.Gray;
+            this.clicked = false;
         }
 
         public void Draw(Graphics graphics)
         {
             Brush brush = new SolidBrush(this.Color);
             Pen pen = new Pen(Color.Black);
-            graphics.FillRectangle(brush, this.X, this.Y, Size, Size);
-            graphics.DrawRectangle(pen, this.X, this.Y, Size, Size);
+
+            if (clicked)
+            {
+
+                Color fullyTransparentColor = Color.FromArgb(0, 0, 0, 0);
+
+                graphics.FillRectangle(new SolidBrush(fullyTransparentColor), this.X + 5, this.Y + 5, Size - 10, Size - 10);
+            }
+
+            else
+            {
+                graphics.FillRectangle(brush, this.X, this.Y, Size, Size);
+                graphics.DrawRectangle(pen, this.X, this.Y, Size, Size);
+            }
+
             brush.Dispose();
+            pen.Dispose();
         }
         public bool IsClicked(int x, int y)
         {
